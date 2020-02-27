30cfba840660d13ef6a612768f68618fa0a32db1
itggot-scott-robinson <60005960+itggot-scott-robinson@users.noreply.github.com>
2020-02-27 10:52:08 +0100

    3e utkast

--- app.rb
+++ app.rb
@@ -18,7 +18,6 @@ end
 
 
 get('/users/register') do
-
     slim(:"users/register")
 end
 
@@ -28,21 +27,22 @@ post('/create') do
     username = params["username"]
     password = params["password"]
     confirm_password = params["confirm_password"]
+    behorighet = 1
     result = db.execute("SELECT * FROM Users WHERE username=?", username)
 
     if result.empty?
         if password == confirm_password
             password_digest = BCrypt::Password.create(password)
-            db.execute("INSERT INTO Users(username, Password) VALUES (?,?)", [username, password_digest])
+            db.execute("INSERT INTO Users(username, Password, behorighet) VALUES (?,?,?)", [username, password_digest, behorighet])
             session[:user_id] = db.execute("SELECT user_id FROM Users WHERE username=?", [username])
             session[:username] = username
             redirect('/register_confirmation')
         else
-            redirect('/error')
+            redirect('/error_not_same_password')
             
         end
     else
-        redirect('/error')
+        redirect('/error_same_username')
     end
 
     redirect('/users/index')
@@ -58,18 +58,22 @@ get('/users/login') do
     slim(:"users/login")
 end
 
+get('/lists/index') do
+    docs = []
+    slim(:"lists/index", locals:{docs:docs})
+end
 
 post('/login') do
     db = connect_to_db("db/databas.db")
     username = params["username"]
     password = params["password"]
     db.results_as_hash = true
-    result = db.execute("SELECT user_id, Password FROM Users WHERE username=?", [username])
+    result = db.execute("SELECT user_id, password FROM Users WHERE username=?", [username])
     if result.empty?
-        redirect('/error')
+        redirect('/error_login')
     end
     user_id = result.first["user_id"]
-    password_digest = result.first["Password"]
+    password_digest = result.first["password"]
     if BCrypt::Password.new(password_digest) == password
         session[:username] = username
         session[:user_id] = user_id
@@ -79,10 +83,10 @@ post('/login') do
 end
 
 
-get('/lists/index') do
+get('/lists/create') do
     db = connect_to_db("db/databas.db")
     db.results_as_hash = true
-    todo = db.execute('SELECT * FROM Lists WHERE user_id=?', session[:user_id])
+    todo = db.execute('SELECT * FROM Doc WHERE doc_id=?', session[:user_id])
     slim(:"lists/index", locals:{docs:docs})
 end
 
@@ -93,7 +97,7 @@ post('/lists/create_list') do
     if title == ""
         redirect('/lists/index')
     end
-    db.execute("INSERT INTO Lists(Title, user_id) VALUES (?,?)", [title, session[:user_id]])
+    db.execute("INSERT INTO Doc(doc_name, doc_id) VALUES (?,?)", [title, session[:user_id]])
     redirect('/lists/index')
 end
 
@@ -102,7 +106,7 @@ post('/lists/:id/update') do
     db = connect_to_db("db/databas.db")
     update=params["update"]
     list_id = params[:id].to_i
-    db.execute('UPDATE Lists SET Title = ? WHERE ID=? ', update, list_id)
+    db.execute('UPDATE Doc SET doc_name = ? WHERE ID=? ', update, list_id)
     redirect('/lists/index')
 end
 
@@ -110,11 +114,23 @@ end
 post('/lists/:id/delete') do
     db = connect_to_db("db/databas.db")
     list_id = params[:id].to_i
-    db.execute('DELETE FROM Lists WHERE user_id=? ', list_id)
+    db.execute('DELETE FROM Doc WHERE doc_id=? ', list_id)
     redirect('/lists/index')
 end
 
 
 get('/error') do
     slim(:error)
+end
+
+get('/error_same_username') do
+    slim(:error_same_username)
+end
+
+get('/error_not_same_password') do
+    slim(:error_not_same_password)
+end
+
+get('/error_login') do
+    slim(:error_login)
 end
